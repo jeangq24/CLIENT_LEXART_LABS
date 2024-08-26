1c78440ca8e07a693af2f453dfb2a39dfac22aed
Jean Garzon <jeangarzon24@gmail.com>
2024-08-26 16:18:47 -0500

    Actualizacion CSP

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: process.env.NODE_ENV ? `default-src 'self'; script-src 'self' 'unsafe-eval'; connect-src 'self' ${process.env.NEXT_PUBLIC_SERVER_HOST} ${process.env.NEXT_PUBLIC_SERVER_HOST_SOCKET} img-src 'self' data:; style-src 'self' 'unsafe-inline';` : `default-src 'self'; script-src 'self'; connect-src 'self' ${process.env.NEXT_PUBLIC_SERVER_HOST} ${process.env.NEXT_PUBLIC_SERVER_HOST_SOCKET}; img-src 'self' data:; style-src 'self' 'unsafe-inline';`,
+            value: process.env.NODE_ENV ? `default-src 'self'; script-src 'self' 'unsafe-eval'; connect-src ${process.env.NEXT_PUBLIC_SERVER_HOST} ${process.env.NEXT_PUBLIC_SERVER_HOST_SOCKET} img-src 'self' data:; style-src 'self' 'unsafe-inline';` : `default-src 'self'; script-src 'self'; connect-src 'self' ${process.env.NEXT_PUBLIC_SERVER_HOST} ${process.env.NEXT_PUBLIC_SERVER_HOST_SOCKET}; img-src 'self' data:; style-src 'self' 'unsafe-inline';`,
           },
         
         ]
